033f1192e1db56f4105aff85f5eed130c9ea847e
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2024-05-13 20:06:05 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,32 +1,4 @@
-library(DoubletFinder)
-data_dir <- 'C://Bioinf/HUMAN_FETAL_RETINA/GW/'
-list.files(data_dir)
-D59fetal <- Read10X(data.dir = data_dir)
-D59fetalS = CreateSeuratObject(counts = D59fetal)
 
-meta <- read.csv('C://Bioinf/HUMAN_FETAL_RETINA/GSE138002_All_barcodes.csv')
-rownames(meta) <- meta$X
-D59fetalS <- AddMetaData(D59fetalS, metadata = meta)
-
-table(D59fetalS$Age)
-
-HGW <- subset(D59fetalS, subset = Age == 'Hgw27')
-
-s.genes <- cc.genes.updated.2019$s.genes
-g2m.genes <- cc.genes.updated.2019$g2m.genes
-
-ProcessSeu <- function(Seurat){
-  Seurat <- NormalizeData(Seurat)
-  Seurat <- FindVariableFeatures(Seurat, selection.method = "vst", nfeatures = 2000)
-  Seurat <- ScaleData(Seurat) #could be replaced with SCTransform
-  Seurat <- RunPCA(Seurat)
-  Seurat <- FindNeighbors(Seurat, dims = 1:50)
-  Seurat <- FindClusters(Seurat, resolution = 1)
-  Seurat <- RunUMAP(Seurat, dims = 1:50)
-  Seurat <- RunTSNE(Seurat,  dims.use = 1:50 )
-  DimPlot(object = Seurat, reduction = "umap")
-  return (Seurat)
-}
 
 RDoublet <- function(tmp){
   sweep.res.list <- paramSweep_v3(tmp, PCs = 1:30, sct = FALSE)
@@ -35,38 +7,51 @@ RDoublet <- function(tmp){
   pKopt <- as.numeric(as.character(bcmvn$pK[bcmvn$BCmetric == max(bcmvn$BCmetric)]))
   pKopt <- pKopt[order(pKopt, decreasing = TRUE) ]
   pKopt <- pKopt[1]
-  homotypic.prop <- modelHomotypic(tmp$seurat_clusters) 
-  nExp_poi <- round(0.1*length(colnames(tmp)))  ## Assuming 10% doublet formation rate 
+  homotypic.prop <- modelHomotypic(tmp$seurat_clusters)
+  nExp_poi <- round(0.05*length(colnames(tmp)))  ## Assuming 10% doublet formation rate
   nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
   tmp <- doubletFinder_v3(tmp, PCs = 1:30, pN = 0.25, pK = pKopt, nExp = nExp_poi, reuse.pANN = FALSE)
   tmp <- doubletFinder_v3(tmp, PCs = 1:30, pN = 0.25, pK = pKopt, nExp = nExp_poi.adj, reuse.pANN = paste("pANN_0.25",pKopt,nExp_poi, sep="_"))
-  return (tmp) 
+  return (tmp)
 }
 
+s.genes <- cc.genes.updated.2019$s.genes
+g2m.genes <- cc.genes.updated.2019$g2m.genes
+ProcessSeu <- function(Seurat){
+  Seurat <- NormalizeData(Seurat)
+  Seurat <- FindVariableFeatures(Seurat, selection.method = "vst", nfeatures = 3000)
+  Seurat <- ScaleData(Seurat) #could be replaced with SCTransform
+  Seurat <- RunPCA(Seurat, npcs = 100)
+  Seurat <- FindNeighbors(Seurat, dims = 1:100)
+  Seurat <- FindClusters(Seurat, resolution = 1)
+  Seurat <- RunUMAP(Seurat, dims = 1:100)
+  Seurat <- RunTSNE(Seurat,  dims.use = 1:100)
+  DimPlot(object = Seurat, reduction = "umap")
+  return (Seurat)
+}
 
-HGW[["percent.rb"]] <- PercentageFeatureSet(HGW, pattern = "^RPS|^RPL|^MRPS|^MRPL", assay = 'RNA')
-HGW[["percent.mt"]] <- PercentageFeatureSet(HGW, pattern = "^MT-")
-HGW <- CellCycleScoring(HGW, s.features = s.genes, g2m.features = g2m.genes, set.ident = FALSE, nbin = 6)
+data_dir <- 'C://Bioinf/HUMAN_FETAL_RETINA/D59_fetal_filtered_gene_bc_matrices/GRCh38'
+list.files(data_dir)
+D59fetal <- Read10X(data.dir = data_dir)
+D59fetalS = CreateSeuratObject(counts = D59fetal)
 
-VlnPlot(HGW, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", 'percent.rb'), ncol = 4)
+D59fetalS[["percent.rb"]] <- PercentageFeatureSet(D59fetalS, pattern = "^RPS|^RPL|^MRPS|^MRPL", assay = 'RNA')
+D59fetalS[["percent.mt"]] <- PercentageFeatureSet(D59fetalS, pattern = "^MT-")
+D59fetalS <- CellCycleScoring(D59fetalS, s.features = s.genes, g2m.features = g2m.genes, set.ident = FALSE)
+VlnPlot(D59fetalS, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", 'percent.rb'), ncol = 4)
+D59fetalS <- subset(D59fetalS, subset = nCount_RNA > 300 & nCount_RNA < 15000 & nFeature_RNA > 500 & nFeature_RNA < 4500 & percent.mt < 15 & percent.rb < 40)
+D59fetalS <- ScaleData(D59fetalS, verbose = T, vars.to.regress = c('percent.mt', "percent.rb","S.Score","G2M.Score"))
 
-HGW <- subset(HGW, subset = nCount_RNA > 300 & nCount_RNA < 15000 & nFeature_RNA > 500 & nFeature_RNA < 4500 & percent.mt < 15 & percent.rb < 40)
-HGW <- ScaleData(HGW, verbose = T, vars.to.regress = c('nCount_RNA', 'percent.mt', "percent.rb","S.Score","G2M.Score")) 
+D59fetalS <- ProcessSeu(D59fetalS)
+D59fetalS <- RDoublet(D59fetalS)
+D59fetalS <- subset(D59fetalS, cells = colnames(D59fetalS )[which(D59fetalS [[]][12] == 'Singlet')])
+D59fetalS <- subset(D59fetalS , cells = colnames(D59fetalS )[which(D59fetalS [[]][13] == 'Singlet')])
+D59fetalS <- ProcessSeu(D59fetalS)
+D59fetalS$origin <- 'Thomas Reh Lab'
+D59fetalS$timepoint <- 'Day 59'
+D59fetalS$tissue <-  'Total retina'
+D59fetalS$donor <- '1'
+SaveH5Seurat(D59fetalS, 'C://Bioinf/HUMAN_FETAL_RETINA/v2/FD59.h5Seurat', overwrite = TRUE)
+saveRDS(D59fetalS, 'C://Bioinf/HUMAN_FETAL_RETINA/v2/FD59.rds')
 
-HGW <- ProcessSeu(HGW)
-
-#Basic visualisation
-FeaturePlot(HGW, features = c('RBPMS'))
-DimPlot(HGW, reduction = 'umap', label = TRUE, repel = TRUE)
-
-HGW <- RDoublet(HGW)
-HGW <- subset(HGW, cells = colnames(HGW )[which(HGW [[]][17] == 'Singlet')])
-HGW <- subset(HGW , cells = colnames(HGW )[which(HGW [[]][18] == 'Singlet')])
-
-HGW <- ProcessSeu(HGW)
-
-HGW$origin <- 'Lu'
-HGW$timepoint <- 'Week 27'
-
-SaveH5Seurat(HGW, 'C://Bioinf/HUMAN_FETAL_RETINA/HGW27.h5Seurat')
-saveRDS(HGW, 'C://Bioinf/HUMAN_FETAL_RETINA/HGW27.rds')
+gc()
